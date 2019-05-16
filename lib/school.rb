a4be4bff3ccfc285797ba12a9ab00b5ddce6fda3
AustinBH <harlowaustin2@gmail.com>
2019-05-15 17:57:05 -0700

    Done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require 'pry'
+class School
+
+  attr_reader :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @grade = grade
+    @name = name
+    #binding.pry
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    new_hash = {}
+    @roster.map {|grade, student| new_hash[grade] = student.sort}
+    new_hash
+  end
+
+end
